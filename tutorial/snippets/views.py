a403f99d9f337057d145c7bf196bf832cdea2248
Stanko Culaja <culaja@gmail.com>
2017-04-04 14:03:03 +0200

    Added highlighting feature but not used yet. Authorization added for the users. Next step is authentication.

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,5 +1,9 @@
+from django.contrib.auth.models import User
+from rest_framework import permissions
+
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.permissions import IsOwnerOrReadOnly
+from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics
 
 
@@ -7,7 +11,22 @@ class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
