7f7d2bbbbc5ad0c32ef990089b482c36f5f043b5
Stanko Culaja <culaja@gmail.com>
2017-04-04 14:22:12 +0200

    Added full hghlighting feature. Now we can access the highlighted code from the browser.

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,5 +1,9 @@
 from django.contrib.auth.models import User
 from rest_framework import permissions
+from rest_framework import renderers
+from rest_framework import reverse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from snippets.models import Snippet
 from snippets.permissions import IsOwnerOrReadOnly
@@ -30,3 +34,18 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = (renderers.StaticHTMLRenderer,)
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
